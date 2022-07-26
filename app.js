9dfb3dad151a6dc93dab1d9280c5fdbdeb7c9727
ncnguyen1314 <ncnguyen1314@gmail.com>
2022-07-26 16:50:56 +0700

    @@

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
 
+//
+app.use(morgan('combined'))
+//route:
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
