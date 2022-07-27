78836147f1e4933129e07190fa2994f639ea35bb
ncnguyen1314 <ncnguyen1314@gmail.com>
2022-07-27 13:55:03 +0700

    @@

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
-
+const path = require("path");
 //
-app.use(morgan('combined'))
-//route:
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars');
+const port = 3000;
+//Morgan HTTP logger:
+app.use(morgan('combined'));
 
+//Template engine:
+
+app.engine('.hbs', exphbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, "views"));
+
+//route:
+app.get("/", (req, res) => {
+	res.render("home");
+});
+//Demo local host
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`);
 })
