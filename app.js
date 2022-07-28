4e41c61ab4394e4f24a95c05d0c5df6f43e29479
ncnguyen1314 <ncnguyen1314@gmail.com>
2022-07-28 14:48:54 +0700

    @@

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ const exphbs  = require('express-handlebars');
 const port = 3000;
 //Morgan HTTP logger:
 app.use(morgan('combined'));
-
+//Static:
+app.use(express.static(path.join(__dirname, "public")));
 //Template engine:
+//exphbs.registerPartials(__dirname + '/views/partials');
 
-app.engine('.hbs', exphbs.engine({extname: '.hbs'}));
+app.engine('.hbs', exphbs.engine({
+  extname: '.hbs',
+  partialsDir:__dirname + '/views/partials',
+}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, "views"));
 
 //route:
 app.get("/", (req, res) => {
+	res.render("home", layout: 'homeside');
+});
+app.get("/shop", (req, res) => {
 	res.render("shop");
+});
+app.get("/product", (req, res) => {
+	res.render("product-details");
 });
 //Demo local host
 app.listen(port, () => {
