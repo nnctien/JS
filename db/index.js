6a70a160e68e4aad02ae4908c68cca85c178aab4
ncnguyen1314 <ncnguyen1314@gmail.com>
2022-08-03 20:42:05 +0700

    connect to db typo

--- db/index.js
+++ db/index.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         await mongoose.connect('mongodb://localhost:27017/RenaeShop_prod'); 
-        console.log('Connect Sucessfully!') 
+        console.log('Connect successfully!') 
     } catch (error) {
-        console.log('Failed tp connect to database!') 
+        console.log('Failed to connect to database!') 
     }
  
 }
