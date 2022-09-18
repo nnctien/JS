afe7cbf57e00fd2ca5fadca1e4faaacb39eab545
ncnguyen1314 <ncnguyen1314@gmail.com>
2022-09-18 13:21:52 +0700

    create conflit

--- jwt.js
+++ jwt.js
@@ -5,5 +5,6 @@ var privateKey = "goaway";
 const accessTokenLife = process.env.ACCESS_TOKEN_LIFE;
 const accessTokenSecret =
   process.env.ACCESS_TOKEN_SECRET ||
-  "access-token-secret-example-trungquandev.com-green-cat-a@";
+  "access-token-secret-example-@";
 console.log(accessTokenLife);
+const a = 1 + 3;
